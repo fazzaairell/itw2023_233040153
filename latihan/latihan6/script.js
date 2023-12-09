eb0b2a031e9181322c87eaa206b39a9ffed6d21f
fazzaairell <fazzaviurra@gmail.com>
2023-12-09 16:37:28 +0700

    mengerjakan tugas 5 dan 6

---
+++ latihan/latihan6/script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.meenu-toggle input');
+const nav = document.querySelector('nav');
+
+menuToggle.addEventListener('click', function() {
+   nav.ClassList.oggle('slide'); 
+});
